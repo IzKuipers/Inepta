ce58c7d7a2d9d2c3340d1432ab33b2c330404fee
Izaak Kuipers <izaak.kuipers@gmail.com>
2024-10-29 21:10:51 +0100

    Remove version information from the login screen

--- src/state/login/login.js
+++ src/state/login/login.js
@@ -1,5 +1,5 @@
 import { LoginApp } from "../../apps/loginapp/metadata.js";
-import { KERNEL, RendererPid, VERSION } from "../../env.js";
+import { RendererPid } from "../../env.js";
 import { loadApp } from "../../js/apps/load.js";
 import { spawnApp } from "../../js/apps/spawn.js";
 import { AppStore } from "../../js/apps/store.js";
@@ -9,10 +9,4 @@ export default async function render() {
 
   await loadApp(LoginApp);
   await spawnApp("loginApp", RendererPid.get(), "SYSTEM");
-
-  const versionNumber = document.querySelector("#stateLoader.login #versionNumber");
-
-  versionNumber.innerText = `Inepta v${VERSION.join(".")}-${KERNEL.BUILD}\n${
-    Object.entries(KERNEL.state.store).length
-  } States | ${KERNEL.modules.length} Kernel Modules`;
 }
