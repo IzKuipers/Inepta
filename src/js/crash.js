f639f40f6823d1dae9db783da4c125ea51f2c60c
Izaak Kuipers <izaak.kuipers@gmail.com>
2024-12-26 00:50:10 +0100

    shit

--- src/js/crash.js
+++ src/js/crash.js
@@ -13,7 +13,7 @@ export function Crash(reason) {
   Log(`Crash`, `### ---![ WE ARE CRASHING! ]!--- ###`, LogType.critical);
   Log(`Crash`, reason.error ? reason.error.message : reason.reason.message, LogType.critical);
 
-  const str = `**** INEPTA EXCEPTION ****\n\nAn error has occured, and Inepta has been halted.\nDetails of the error and the system log can be found below.\nNewest log entry is at the top.\n\nIf this keeps happening, try unloading any sideloaded applications.\n\n`;
+  const str = `**** INEPTA EXCEPTION ****\n\nAn error has occurred, and Inepta has been halted.\nDetails of the error and the system log can be found below.\nNewest log entry is at the top.\n\nIf this keeps happening, try unloading any sideloaded applications.\n\n`;
   const stack = reason.error ? reason.error.stack : reason.reason.stack;
 
   let text = str;
